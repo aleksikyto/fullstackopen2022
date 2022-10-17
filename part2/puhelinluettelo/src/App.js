249f5849aa8024eb3a5c2aadd3c6596ebcdd2efd
aleksikyto <aleksi.kyto@hotmail.com>
2022-10-17 18:05:25 +0300

    puhelinluettelo step1 done

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -4,30 +4,38 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    const personObject = {
+      name: newName,
+    };
+
+    setPersons((persons) => [...persons, personObject]);
+    setNewName("");
+  };
+
+  const handleChange = (event) => {
+    setNewName(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange} />
         </div>
-        <div>debug: {newName}</div>
         <div>
-          <button
-            type="submit"
-            onClick={() => {
-              // add functionality
-            }}
-          >
-            add
-          </button>
+          <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => {
-          return person.name;
-        })}
+        {persons.length >= 0 &&
+          persons.map((person, i) => {
+            return <li key={i}> {person.name} </li>;
+          })}
       </div>
     </div>
   );
