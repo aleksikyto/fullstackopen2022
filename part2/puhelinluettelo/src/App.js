aa7c8730b128424bfb51c72cfd46ece17484ed6d
aleksikyto <aleksi.kyto@metropolia.com>
2022-10-17 15:15:03 +0300

    puhelinluettelo base added

---
+++ part2/puhelinluettelo/src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>debug: {newName}</div>
+        <div>
+          <button
+            type="submit"
+            onClick={() => {
+              // add functionality
+            }}
+          >
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => {
+          return person.name;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
